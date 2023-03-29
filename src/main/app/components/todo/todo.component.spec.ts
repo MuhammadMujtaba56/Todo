19313858aa77787425f3a68eaf1d2275ad813056
Muhammad Mujtaba <muhammad.mujtaba@techno-soft.com>
2023-03-29 17:07:03 +0500

    chore: linting added, px replaced in css and other bugs fixed

--- src/main/app/components/todo/todo.component.spec.ts
+++ src/main/app/components/todo/todo.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TodoComponent } from './todo.component';
 
@@ -9,6 +10,7 @@ describe('TodoComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TodoComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TodoComponent);
--- src/main/environments/environment.prod.ts
+++ src/main/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  url: 'https://642416ea474017404332c39d.mockapi.io/list',
 };
